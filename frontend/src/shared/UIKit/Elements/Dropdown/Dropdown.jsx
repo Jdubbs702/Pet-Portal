e64934960298de33ab331ee62c972f507a7750f4
Jdubbs702 <702leopold@gmail.com>
2022-09-04 08:16:54 +0300

    various style changes

--- frontend/src/shared/UIKit/Elements/Dropdown/Dropdown.jsx
+++ frontend/src/shared/UIKit/Elements/Dropdown/Dropdown.jsx
@@ -1,71 +1,87 @@
 import { useEffect, useState } from "react";
 import { Icon, Line } from "../../../UIKit";
-import './Dropdown.css'
+import "./Dropdown.css";
 
 const Dropdown = ({ list, width, text, selectedItem, onSelect }) => {
-    const [isOpen, setisOpen] = useState(false);
+  const [isOpen, setisOpen] = useState(false);
 
-    useEffect(() => {
-        document.body.addEventListener('click', handleBodyClick);
-        return () => { //triggers once on unmount
-            document.body.removeEventListener('click', handleBodyClick);
-        }
-    }, []);//[] triggers once on mount
-
-    const handleBodyClick = () => {
-        setisOpen(false);
+  useEffect(() => {
+    document.body.addEventListener("click", handleBodyClick);
+    return () => {
+      //triggers once on unmount
+      document.body.removeEventListener("click", handleBodyClick);
     };
+  }, []); //[] triggers once on mount
 
-    const triggerHandler = (e) => {
-        console.log(e.type);
+  const handleBodyClick = () => {
+    setisOpen(false);
+  };
 
-        e.nativeEvent.stopImmediatePropagation();
-        setisOpen(!isOpen);
-    };
+  const triggerHandler = (e) => {
+    console.log(e.type);
 
-    const selectHandler = (i) => {
-        onSelect(i);
-        setisOpen(false);
-    };
+    e.nativeEvent.stopImmediatePropagation();
+    setisOpen(!isOpen);
+  };
 
-    const getSelected = () => {
-        if (selectedItem) {
-            const selected = list.find(i => i.id === selectedItem.id);
-            return selected.value
-        };
-        return text;
-    };
+  const selectHandler = (i) => {
+    onSelect(i);
+    setisOpen(false);
+  };
 
-    const clearSelection = () => {
-        onSelect('');
-    };
+  const getSelected = () => {
+    if (selectedItem) {
+      const selected = list.find((i) => i.id === selectedItem.id);
+      return selected.value;
+    }
+    return text;
+  };
 
-    return (
-        <div className="Dropdown" style={width} >
-            <div className='trigger' onClick={triggerHandler}>
-                <Line between className='trigger-line'>
-                    <div className="selector-text">{getSelected()}</div>
-                    <Icon i="chevron-down" />
-                </Line>
+  const clearSelection = () => {
+    onSelect("");
+  };
+
+  return (
+    <div className="Dropdown" style={width}>
+      <div className="trigger" onClick={triggerHandler}>
+        <Line between className="trigger-line">
+          <div className="selector-text">{getSelected()}</div>
+          <Icon i="chevron-down" />
+        </Line>
+      </div>
+      {isOpen && selectedItem && (
+        <div className="list">
+          <div onClick={clearSelection}>--clear selection--</div>
+          {list.map((i) => (
+            <div
+              className={i.id === selectedItem.id ? "selected" : ""}
+              key={i.id}
+              onClick={() => {
+                selectHandler(i);
+              }}
+            >
+              {i.value}
             </div>
-            {isOpen && selectedItem &&
-                <div className="list">
-                    <div onClick={clearSelection}>--clear selection--</div>
-                    {list.map(i => (
-                        <div className={i.id === selectedItem.id ? 'selected' : ''} key={i.id} onClick={() => { selectHandler(i) }}>{i.value}</div>
-                    ))}
-                </div>
-            }
-            {(isOpen && !selectedItem) &&
-                <div className="list">
-                    <div onClick={clearSelection}>--clear selection--</div>
-                    {list.map(i => (
-                        <div key={i.id} onClick={() => { selectHandler(i) }}>{i.value}</div>
-                    ))}
-                </div>
-            }
+          ))}
         </div>
-    );
+      )}
+      {isOpen && !selectedItem && (
+        <div className="list">
+          <div onClick={clearSelection}>--clear--</div>
+          {list.map((i) => (
+            <div
+              key={i.id}
+              onClick={() => {
+                selectHandler(i);
+              }}
+            >
+              {i.value}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
